44820542dff399b49a57b1dfa6b75d98089c8b0c
Ivo Zarkov <muletokoetoraboti@gmail.com>
2020-01-25 16:34:38 +0200

    README

---
+++ README.md
@@ -0,0 +1,102 @@
+# tokio-postgres-mapper
+
+`tokio_postgres-mapper` is a proc-macro designed to make mapping from postgresql
+tables to structs simple.
+
+### Why?
+
+It can be frustrating to write a lot of boilerplate and, ultimately, duplicated
+code for mapping from postgres Rows into structs.
+
+For example, this might be what someone would normally write:
+
+```rust
+extern crate postgres;
+
+use postgres::rows::Row;
+
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub email: Option<String>,
+}
+
+impl From<Row> for User {
+    fn from(row: Row) -> Self {
+        Self {
+            id: row.get("id"),
+            name: row.get("name"),
+            email: row.get("email"),
+        }
+    }
+}
+
+// code to execute a query here and get back a row
+let user = User::from(row); // this can panic
+```
+
+This becomes worse when manually implementating using the non-panicking
+`get_opt` method variant.
+
+Using this crate, the boilerplate is removed, and panicking and non-panicking
+implementations are derived:
+
+```rust
+#[macro_use] extern crate tokio_postgres_mapper_derive;
+extern crate tokio_postgres_mapper;
+
+use tokio_postgres_mapper::FromPostgresRow;
+
+#[derive(PostgresMapper)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub email: Option<String>,
+}
+
+// Code to execute a query here and get back a row might now look like:
+let stmt = "SELECT * FROM user WHERE username = $1 AND password = $2";
+
+let user = client.query_one(stmt, &[&5, "asdf"]).await?;
+let user = User::from(user);
+
+```
+
+### The two crates
+
+This repository contains two crates: `postgres-mapper` which contains an `Error`
+enum and traits for converting from a `postgres` or `tokio-postgres` `Row`
+without panicking, and `postgres-mapper-derive` which contains the proc-macro.
+
+`postgres-mapper-derive` has 3 features that can be enabled (where T is the
+struct being derived with the provided `PostgresMapper` proc-macro):
+
+- `postgres-support`, which derives
+  `impl<'a> From<::postgres::rows::Row<'a>> for T` and
+  `impl<'a> From<&'a ::postgres::Row<'a>> for T` implementations
+- `tokio-postgres-support`, which derives
+  `impl From<::tokio_postgres::rows::Row> for T` and
+  `impl From<&::tokio_postgres::rows::Row> for T` implementations
+- `tokio-postgres-mapper` which, for each of the above features, implements
+  `tokio-postgres-mapper`'s `FromPostgresRow` and/or `FromTokioPostgresRow` traits
+
+`tokio-postgres-mapper` has two features, `postgres-support` and
+`tokio-postgres-support`. When one is enabled in `tokio-postgres-mapper-derive`, it
+must also be enabled in `tokio-postgres-mapper`.
+
+### Installation
+
+Add the following to your `Cargo.toml`:
+
+```toml
+tokio-postgres-mapper = { git = "https://github.com/EasyS7/psql-mapper" }
+tokio-postgres-mapper-derive = { git = "https://github.com/EasyS7/psql-mapper" }
+```
+
+This will derive implementations for converting from owned and referenced
+`tokio-postgres::rows::Row`s, as well as implementing `tokio-postgres-mapper`'s
+`FromTokioPostgresRow` trait for non-panicking conversions.
+
+### License
+
+ISC.
---
+++ postgres_mapper_derive/src/lib.rs
@@ -0,0 +1,147 @@
+extern crate proc_macro;
+#[macro_use]
+extern crate quote;
+extern crate syn;
+
+use proc_macro::TokenStream;
+
+use syn::{
+    Data, DataStruct, DeriveInput, GenericParam, Ident, ImplGenerics, Item, Lifetime, LifetimeDef,
+    TypeGenerics, WhereClause,
+};
+
+#[proc_macro_derive(PostgresMapper)]
+pub fn postgres_mapper(input: TokenStream) -> TokenStream {
+    let mut ast: DeriveInput = syn::parse(input).expect("Couldn't parse item");
+
+    impl_derive(&mut ast)
+}
+
+fn impl_derive(ast: &mut DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
+
+    let s = match ast.data {
+        Data::Struct(ref s) => s,
+        _ => panic!("Enums or Unions can not be mapped"),
+    };
+
+    let from_row = impl_tokio_from_row(s, name, &impl_generics, &ty_generics, &where_clause);
+
+    let tokio_postgres_mapper =
+        impl_tokio_postgres_mapper(s, name, impl_generics, ty_generics, where_clause);
+
+    let mut g = ast.generics.clone();
+
+    g.params.push(GenericParam::Lifetime(LifetimeDef::new(
+        syn::parse_str::<Lifetime>("'DERIVE").unwrap(),
+    )));
+    let (impl_generics, _, _) = { g.split_for_impl() };
+
+    let from_row_borrowed =
+        impl_tokio_from_row_ref(s, name, &impl_generics, ty_generics, where_clause);
+
+    let tokens = quote! {
+        #from_row
+        #from_row_borrowed
+        #tokio_postgres_mapper
+    };
+
+    tokens.into()
+}
+
+fn impl_tokio_from_row(
+    s: &DataStruct,
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    ty_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> Item {
+    let fields = s.fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+
+        let row_expr = format!(r##"{}"##, ident);
+        quote! {
+            #ident:row.get(#row_expr)
+        }
+    });
+
+    let tokens = quote! {
+        impl #impl_generics From<tokio_postgres::row::Row> for #name #ty_generics #where_clause  {
+            fn from(row:tokio_postgres::row::Row) -> Self {
+                Self {
+                    #(#fields),*
+                }
+            }
+        }
+    };
+
+    syn::parse_quote!(#tokens)
+}
+
+fn impl_tokio_from_row_ref(
+    s: &DataStruct,
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    ty_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> Item {
+    let fields = s.fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+
+        let row_expr = format!(r##"{}"##, ident);
+        quote! {
+            #ident:row.get(#row_expr)
+        }
+    });
+
+    let tokens = quote! {
+        impl #impl_generics From<&'DERIVE tokio_postgres::row::Row> for #name #ty_generics #where_clause {
+            fn from(row:&'DERIVE tokio_postgres::row::Row) -> Self {
+                Self {
+                    #(#fields),*
+                }
+            }
+        }
+    };
+
+    syn::parse_quote!(#tokens)
+}
+
+fn impl_tokio_postgres_mapper(
+    s: &DataStruct,
+    name: &Ident,
+    impl_generics: &ImplGenerics,
+    ty_generics: &TypeGenerics,
+    where_clause: &Option<&WhereClause>,
+) -> Item {
+    let fields = s.fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+
+        let row_expr = format!(r##"{}"##, ident);
+        quote! {
+            #ident:row.try_get::<&str,#ty>(#row_expr)?
+        }
+    });
+
+    let fields_copy = fields.clone();
+
+    let tokens = quote! {
+        impl #impl_generics tokio_postgres_mapper::FromTokioPostgresRow for #name #ty_generics #where_clause {
+            fn from_tokio_postgres_row(row: tokio_postgres::row::Row) -> Result<Self, tokio_postgres_mapper::Error> {
+                Ok(Self {
+                    #(#fields),*
+                })
+            }
+
+            fn from_tokio_postgres_row_ref(row: &tokio_postgres::row::Row) -> Result<Self, tokio_postgres_mapper::Error> {
+                Ok(Self {
+                    #(#fields_copy),*
+                })
+            }
+        }
+    };
+
+    syn::parse_quote!(#tokens)
+}
